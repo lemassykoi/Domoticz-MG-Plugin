b7c212ce61a7c3627187e4a20cf6fe5cc666ad4f
Clément PAPPALARDO <16377344+lemassykoi@users.noreply.github.com>
2025-09-05 12:47:20 +0200

    Update plugin.py
    code_to_selector fix

--- plugin.py
+++ plugin.py
@@ -393,7 +393,7 @@ def update_devices(vehicle_data):
         
         # Charge Limit Selector
         if 8 in Devices and charging_status and not car_sleeping:
-            code_to_selector = {0: 0, 1: 40, 2: 50, 3: 60, 4: 70, 5: 80, 6: 90, 7: 100}
+            code_to_selector = {0: 0, 1: 10, 2: 20, 3: 30, 4: 40, 5: 50, 6: 60, 7: 70}
             limit_code = getattr(charging_status.chrgMgmtData, 'bmsOnBdChrgTrgtSOCDspCmd', None)
             if limit_code in code_to_selector:
                 Devices[8].Update(nValue=code_to_selector[limit_code], sValue=str(code_to_selector[limit_code]))
